c9b287b71f79e4e05d176394918fed26fbff9d79
Linus Yan <lyan2@ebay.com>
2017-11-29 14:54:44 +0800

    add App

---
+++ src/main/java/com/linus/spring/bootstrap/demo/App.java
@@ -0,0 +1,12 @@
+package com.linus.spring.bootstrap.demo;
+
+import org.springframework.boot.SpringApplication;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(App.class, args);
+	}
+
+}
